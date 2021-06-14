4d70154d5658a347990866bfe11911a52366a1fb
Dylan Sackmann <dylansackmann+gh@gmail.com>
2021-06-14 09:56:35 -0700

    Passing simple tests

--- uncle/peakFinder.js
+++ uncle/peakFinder.js
@@ -16,24 +16,33 @@ function peakFinder(searchSpace, startingIdx = 0, longestKnownPeak = 0) {
 
 	while (_notNil(step1) && _notNil(step2) && step2 > step1) {
 		// we're on an upslope. 
+		console.log('Upslope ' + step1 + ' ' + step2);
 		candidateLength++;
-		step1 = step2;
-		step2 = searchSpace[++i];
+		step1 = searchSpace[i++];
+		step2 = searchSpace[i];
 	}
 	// We've hit a peak, or the end of the list
 
+	console.log('Peaked: ' + step1 + ' ' + step2)
 	if (!_notNil(step2)) {
 		// case where we were on an upslope, but encountered the end of the list before a downslope began. 
 		// This does not count as a peak, so return 0 instead of the candidateLength
+		console.log('Ended at a peak');
 		return 0;
 	}
 
 	while (_notNil(step1) && _notNil(step2) && step2 < step1) {
-		// we're on an downslope.
+		// we're on an downslope.		
+		console.log('Downslope ' + step1 + ' ' + step2);
+
 		candidateLength++; 
-		step1 = step2;
-		step2 = searchSpace[++i];
+		step1 = searchSpace[i++];
+		step2 = searchSpace[i];
 	}
+
+	console.log('Recursive call with values: ');
+	console.log('startingIdx: ' + startingIdx);
+	console.log('candidateLength: ' + candidateLength);
 
 	return Math.max(peakFinder(searchSpace, i, candidateLength), candidateLength);
 }
