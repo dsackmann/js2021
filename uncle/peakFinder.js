aeeae3add2462ec07b6fbbfa53dc808f9b7ebd54
Dylan Sackmann <dylansackmann+gh@gmail.com>
2021-06-14 11:07:52 -0700

    Test biggish arrays

--- uncle/peakFinder.js
+++ uncle/peakFinder.js
@@ -1,6 +1,9 @@
 
 
 const _notNil = (v) => v !== null && v !== undefined;  
+
+const _isNil = (v) => v === null || v === undefined;  
+
 /*
 Params: 
 	Array<Integer> searchSpace 
@@ -14,7 +17,17 @@ function peakFinder(searchSpace, startingIdx = 0, longestKnownPeak = 0) {
 	let step1 = searchSpace[i];
 	let step2 = searchSpace[i + 1];
 
-	while (_notNil(step1) && _notNil(step2) && step2 > step1) {
+	if (_isNil(step1) || _isNil(step2)) {
+		return candidateLength;
+	}
+
+	if (step1 >= step2) {
+		//This isn't the beginning of an upslope. Increment starting point and move on
+		return peakFinder(searchSpace, i + 1, candidateLength)
+	}
+
+
+	while (step2 > step1) {
 		// we're on an upslope. 
 		console.log('Upslope ' + step1 + ' ' + step2);
 		candidateLength++;
@@ -24,14 +37,15 @@ function peakFinder(searchSpace, startingIdx = 0, longestKnownPeak = 0) {
 	// We've hit a peak, or the end of the list
 
 	console.log('Peaked: ' + step1 + ' ' + step2)
-	if (!_notNil(step2)) {
+	if (_isNil(step2)) {
 		// case where we were on an upslope, but encountered the end of the list before a downslope began. 
 		// This does not count as a peak, so return 0 instead of the candidateLength
 		console.log('Ended at a peak');
 		return 0;
 	}
 
-	while (_notNil(step1) && _notNil(step2) && step2 < step1) {
+
+	while (step2 < step1) {
 		// we're on an downslope.		
 		console.log('Downslope ' + step1 + ' ' + step2);
 
@@ -41,7 +55,7 @@ function peakFinder(searchSpace, startingIdx = 0, longestKnownPeak = 0) {
 	}
 
 	console.log('Recursive call with values: ');
-	console.log('startingIdx: ' + startingIdx);
+	console.log('startingIdx: ' + i);
 	console.log('candidateLength: ' + candidateLength);
 
 	return Math.max(peakFinder(searchSpace, i, candidateLength), candidateLength);
--- uncle/test.js
+++ uncle/test.js
@@ -16,13 +16,60 @@ const assertEqual = (actual, expected, name) =>
 		if (mode !== 'warn') fail();
 }
 
+// Create large arrays full of zeroes an also subsequences inserted at particular positions
+const getBigArray = (size, subSequences =[]) => {
+	const a = [];
+	for (let i = 0; i < size; i++) {
+		let subSeq = subSequences.find(({start, contents}) => i >= start && i < start + contents.length);
+		let val = subSeq ? subSeq.contents[i - subSeq.start] : 0; 
+
+		// if (val) console.log('Inserting ' + val + ' at position ' + i);
+		a.push(val);
+	}
+
+	return a;
+}
+
+let twoPeaks = [
+	{
+		start: 5,
+		contents: [1,2,3,4,5,6,5,4]
+	},
+	{
+		start: 600,
+		contents: [1,2,3,4,5,6,5,4]
+	
+	}
+]
+
+const a = getBigArray(1000, twoPeaks);
+
+assertEqual(peakFinder(a), 10, 'Big 1');
+
+assertEqual(peakFinder(
+		getBigArray(1000, [
+	{
+		start: 5,
+		contents: [1,2,3,4,5,6,5,4]
+	},
+	{
+		start: 600,
+		contents: [1,2,3,4,5,6,5,4,3,2,1]
+	
+	}
+])
+	), 13, 'Big 2');
+
+
+assertEqual(peakFinder(getBigArray(10)), 0, 'Big 3');
+
+
 assertEqual(peakFinder([]), 0, 'Empty');
 
 
 assertEqual(peakFinder([1,2,3,2,1]), 5, 'Simple 1');
 assertEqual(peakFinder([1,2,3,2,1,2,1]), 5, 'Simple 2');
 assertEqual(peakFinder([1,2,1]), 3, 'Simple 3');
-
 
 
 console.log('Test complete');
