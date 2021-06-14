8341b9f9390295ca53318c4222ba086f87e51a41
Dylan Sackmann <dylansackmann+gh@gmail.com>
2021-06-14 09:47:59 -0700

    Simple tests

--- uncle/test.js
+++ uncle/test.js
@@ -6,7 +6,7 @@ const fail = () => process.exit(1);
 const assertEqual = (actual, expected, name) =>
 	{
 		if (expected === actual) {
-			console.log('Passed: ' + name);
+			console.log('Passed: ' + name + '\n');
 			return;
 		}
 
@@ -16,6 +16,13 @@ const assertEqual = (actual, expected, name) =>
 		if (mode !== 'warn') fail();
 }
 
-assertEqual(peakFinder([1,2,3,2,1]), 5);
+assertEqual(peakFinder([]), 0, 'Empty');
+
+
+assertEqual(peakFinder([1,2,3,2,1]), 5, 'Simple 1');
+assertEqual(peakFinder([1,2,3,2,1,2,1]), 5, 'Simple 2');
+assertEqual(peakFinder([1,2,1]), 3, 'Simple 3');
+
+
 
 console.log('Test complete');
